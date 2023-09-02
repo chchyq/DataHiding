4c4477507c295abcabebae6ca54f0263a9f707fd
konbra99 <krazi2255@gmail.com>
2023-09-02 10:36:37 +0200

    better idea

--- src/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main {
-    public static void main(String[] args) {
-        BGNEncryption b = new BGNEncryption();
-        PublicKey PK = b.gen(8);//20
-        b.Test_1(PK, b, 0);
-    }
-}
