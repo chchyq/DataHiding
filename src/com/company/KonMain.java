adcd9f263587de2bc2863d0cbf05ffce9fe37cb4
konbra99 <krazi2255@gmail.com>
2023-09-03 16:29:06 +0200

    maybe

--- src/com/company/KonMain.java
+++ src/com/company/KonMain.java
@@ -1,14 +1,17 @@
 package com.company;
 
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
 
 public class KonMain {
-    public static void main(String[] args) {
-        SecureRandom random = new SecureRandom(new byte[]{0x01});
+    public static void main(String[] args) throws NoSuchAlgorithmException, NoSuchProviderException {
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        random.setSeed(new byte[]{0x01});
 
         System.out.print("Testing nextInt()..");
-        int[] expected = {417885477, -1770363278, -364100061, -923580003, -1116182179, -512665530, 1024819304, 523492671};
+        int[] expected = {170601090, 1365618327, 1120287500, 1843067938};
         for (int value : expected) {
             int generated = random.nextInt();
             if (generated != value) {
@@ -18,7 +21,7 @@ public class KonMain {
         System.out.println(" works :)");
 
         System.out.print("Testing probablePrime()..");
-        int[] primes = {251, 151, 139, 233, 241, 229, 251, 151};
+        int[] primes = {223, 157, 137, 199};
         for (int prime : primes) {
             BigInteger generated = BigInteger.probablePrime(8, random);
             if (!generated.equals(BigInteger.valueOf(prime))) {
