7bb30a08fbc3eeff17d36d78cedb66ceb90c9422
konbra99 <krazi2255@gmail.com>
2023-09-03 14:12:01 +0200

    random tests

--- src/com/company/KonMain.java
+++ src/com/company/KonMain.java
@@ -1,12 +1,30 @@
 package com.company;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class KonMain {
     public static void main(String[] args) {
-        BGNEncryption b = new BGNEncryption();
-        PublicKey PK = b.gen(8); // 20
+        SecureRandom random = new SecureRandom(new byte[]{0x01});
 
-        System.out.println("Running Test!");
-        b.Test_1(PK, 0);
+        System.out.print("Testing nextInt()..");
+        int[] expected = {417885477, -1770363278, -364100061, -923580003, -1116182179, -512665530, 1024819304, 523492671};
+        for (int value : expected) {
+            int generated = random.nextInt();
+            if (generated != value) {
+                throw new RuntimeException("Nope! " + generated + " != " + value);
+            }
+        }
+        System.out.println(" works :)");
 
+        System.out.print("Testing probablePrime()..");
+        int[] primes = {251, 151, 139, 233, 241, 229, 251, 151};
+        for (int prime : primes) {
+            BigInteger generated = BigInteger.probablePrime(8, random);
+            if (!generated.equals(BigInteger.valueOf(prime))) {
+                throw new RuntimeException("Nope! " + generated + " != " + prime);
+            }
+        }
+        System.out.println(" works :)");
     }
 }
